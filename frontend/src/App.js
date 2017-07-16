36d2b776d077baa86caaaca74d838e04de9d786e
Sukhatouch Denkirati <sukhatouch.den@gmail.com>
2017-07-16 23:59:05 +0700

    testing sidenav fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SideNav, {MenuIcon} from 'react-simple-sidenav';
 
 class App extends Component {
+    constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  };
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,9 +38,28 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        
+        <MenuIcon onClick={() => this.setState({showNav: true})} className="toggler" />
+          <SideNav
+              showNav        =  {this.state.showNav}
+              onHideNav      =  {() => this.setState({showNav: false})}
+              title          =  "Hello World"
+              items          =  {['home', 'services', 'about', 'contact']}
+              titleStyle     =  {{backgroundColor: '#4CAF50'}}
+              itemStyle      =  {{backgroundColor: '#fff'}}
+              itemHoverStyle =  {{backgroundColor: '#CDDC39'}}
+          />
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
